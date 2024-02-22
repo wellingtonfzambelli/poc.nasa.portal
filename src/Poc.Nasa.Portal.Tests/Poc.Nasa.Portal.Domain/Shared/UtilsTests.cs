cd389ff7cdd3d62d334f9df1b45819c72e236a0e
Wellington M. Fernandes <wellington_fernands@yahoo.com.br>
2024-02-22 11:57:54 -0300

    unit tests enhancements

--- src/Poc.Nasa.Portal.Tests/Poc.Nasa.Portal.Domain/Shared/UtilsTests.cs
+++ src/Poc.Nasa.Portal.Tests/Poc.Nasa.Portal.Domain/Shared/UtilsTests.cs
@@ -5,10 +5,10 @@ namespace Poc.Nasa.Portal.Tests.Poc.Nasa.Portal.Domain.Shared;
 public sealed class UtilsTests
 {
     [Theory]
-    [InlineData("Test1 Test1 Test1 Test1 Test1 Test1 Test1 Test1 Test1 Test1T", 60)]
-    [InlineData("Test1 Test1 Test1", 35)]
-    [InlineData("Test1 Test1 Test1 Test1 Test1", 40)]
-    [InlineData("Test1 Test1 Test1 Test1 Test1 Test1 Test1 Test1 Test1 Test1T Test1 Test1 Test1 Test1T", 85)]
+    [InlineData("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed", 60)]
+    [InlineData("Lorem ipsum dolor", 35)]
+    [InlineData("Lorem ipsum dolor sit amet, c", 40)]
+    [InlineData("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidi", 85)]
     public async Task Validate_Texts_Under_Limit_Will_Not_Truncate_And_Return_Success(string text, int limitChar)
     {
         // Act
@@ -20,10 +20,10 @@ public sealed class UtilsTests
     }
 
     [Theory]
-    [InlineData("Test1 Test1 Test1 Test1 Test1 Test1 Test1 Test1 Test1 Test1T", 59)]
-    [InlineData("Test1 Test1 Test1", 10)]
-    [InlineData("Test1 Test1 Test1 Test1 Test1", 15)]
-    [InlineData("Test1 Test1 Test1 Test1 Test1 Test1 Test1 Test1 Test1 Test1T Test1 Test1 Test1 Test1", 80)]
+    [InlineData("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed", 59)]
+    [InlineData("Lorem ipsum dolor sit", 10)]
+    [InlineData("Lorem ipsum dolor sit amet", 15)]
+    [InlineData("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt", 80)]
     public async Task Validate_Texts_Under_Limit_Will_Be_Truncate_With_Specified_Size_And_Return_Success(string text, int limitChar)
     {
         // Act

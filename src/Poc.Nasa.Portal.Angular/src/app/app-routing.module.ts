42ba6df75f75421f714227e4725dd7f6dad2dc8e
Wellington M. Fernandes <wellington_fernands@yahoo.com.br>
2024-03-07 21:51:26 -0300

    adding menu and header angular

--- src/Poc.Nasa.Portal.Angular/src/app/app-routing.module.ts
+++ src/Poc.Nasa.Portal.Angular/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PictureofthedayComponent } from './pages/pictureoftheday/pictureoftheday.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'pictureoftheday', component: PictureofthedayComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/Poc.Nasa.Portal.Angular/src/app/app.module.ts
+++ src/Poc.Nasa.Portal.Angular/src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { PictureofthedayComponent } from './pages/pictureoftheday/pictureoftheday.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PictureofthedayComponent
+    PictureofthedayComponent,
+    DashboardComponent,
+    NavbarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
